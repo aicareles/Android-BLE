fe13e700183228b3439c108ead61414e806cab43
jerry <823581722@qq.com>
2019-07-26 14:01:14 +0800

    修复Android8.0手机后台扫描问题

--- blelibrary/src/main/java/cn/com/heaton/blelibrary/ble/request/ScanRequest.java
+++ blelibrary/src/main/java/cn/com/heaton/blelibrary/ble/request/ScanRequest.java
@@ -14,6 +14,7 @@ import android.support.annotation.RequiresApi;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import cn.com.heaton.blelibrary.ble.Ble;
 import cn.com.heaton.blelibrary.ble.callback.BleStatusCallback;
@@ -58,6 +59,10 @@ public class ScanRequest<T extends BleDevice> {
                     .build();
             mScannerCallback  = new BLEScanCallback();
             mFilters = new ArrayList<>();
+            UUID uuidService = Ble.options().getUuidService();
+            mFilters.add(new ScanFilter.Builder()
+                    .setServiceUuid(ParcelUuid.fromString(uuidService.toString()))//8.0以上手机后台扫描，必须开启
+                    .build());
         }
         if (mBleObserver == null){
             this.mBleObserver = new BluetoothChangedObserver(Ble.getInstance().getContext());
