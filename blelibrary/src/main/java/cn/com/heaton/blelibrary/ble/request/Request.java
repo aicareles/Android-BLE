df14503c44c59d3e16f58421b396fa51f7fdc313
aicareles <823581722@qq.com>
2019-09-05 16:07:51 +0800

    修改日志TAG接口

---
+++ blelibrary/src/main/java/cn/com/heaton/blelibrary/ble/request/Request.java
@@ -0,0 +1,12 @@
+package cn.com.heaton.blelibrary.ble.request;
+
+import android.os.Message;
+
+/**
+ *
+ * Created by LiuLei on 2017/12/28.
+ */
+
+public interface Request {
+    void handleMessage(Message msg);
+}
